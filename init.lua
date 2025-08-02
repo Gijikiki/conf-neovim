f38094eae25848e3b062fb8c5b22556f88abcf50
Jesse Meyer <twincitiesjesse@gmail.com>
2025-08-02 14:59:56 -0500

    Improve comments in init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,28 @@
 -- Originally from kickstart.nvim
---  Took that as an example when I first started switched from vim to neovim, but this has been heavily modified
+--  Took that as an example when I first started switched from vim to neovim,
+--    but this has been heavily modified
+
+-- Load core settings and configurations
 require("core")
 
+-- Bootstrap the lazy plugin manager, must come before lazy
 require("lazy_bootstrap")
+
+-- Load and configure lazy UI for plugin setup
 local lazy_ui = require("lazy_config.ui")
 require("lazy").setup(
   {
-    { import = "themes" },
-    { import = "plugins" },
+    { import = "themes" }, -- Import theme-related plugins
+    { import = "plugins" }, -- Import general plugins
   },
-  { ui = lazy_ui }
+  { ui = lazy_ui } -- Pass lazy UI settings
 )
+
+-- Define custom key mappings
 require("keymaps")
+
+-- Configure custom filetypes
 require("filetypes")
+
+-- Set up autocommands
 require("autocmds")
